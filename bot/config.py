ed324ccb8bc55987f9beff50653a2bb6b73ab2d9
Suraj0-12 <116282314+Suraj0-12@users.noreply.github.com>
2023-02-18 20:15:22 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -8,7 +8,7 @@ try:
     API_HASH = "04c049aa96d1cc87920b45b7fb43c0d0"
     BOT_TOKEN = "6124192107:AAFZVBavdjJ5UdZWtDpLHs9_Ochi4p9G2kA"
     DEV = "5585763218"
-    OWNER = "5585763218"
+    OWNER = "1427670935"
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMBNAIL = "https://telegra.ph/file/8228dba621eb9da7737b7.jpg"
 except Exception as e:
